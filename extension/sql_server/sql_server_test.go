ab07a9355ee381ef31cdea8b058139fe6d0376be
zhh2024 <15536508342@163.com>
2024-11-06 17:10:13 +0800

    新增查询完的数据渲染格式

--- extension/sql_server/sql_server_test.go
+++ extension/sql_server/sql_server_test.go
@@ -1,10 +1,27 @@
-package sql
+package sql_server
 
 import (
 	"basic/tool/db"
 	"fmt"
 	"testing"
 )
+
+func TestDoHandler(t *testing.T) {
+	params := map[string]any{
+		"host":       "127.0.0.1",
+		"port":       5432,
+		"username":   "postgres",
+		"password":   "postgre",
+		"dbname":     "postgres",
+		"searchPath": "public",
+		"sql":        "select * from test",
+	}
+	instance := GetInstance()
+	resp := instance.Do(params)
+	if resp != nil {
+		fmt.Println(string(resp))
+	}
+}
 
 func GetDb() *dbtool.BaseDb {
 	dbConfig := dbtool.DbConfig{Host: "127.0.0.1", Port: 5432, Username: "postgres", Password: "postgre", Dbname: "postgres", SearchPath: "public"}
