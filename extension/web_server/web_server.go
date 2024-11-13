693424dba65ba4f04ff189b4f3042f8f038223de
changdongliang0121 <2319533864@qq.com>
2024-11-13 11:12:29 +0800

    web组件新增转发

--- extension/web_server/web_server.go
+++ extension/web_server/web_server.go
@@ -5,6 +5,9 @@ import (
 	"basic/tool/net"
 	othertool "basic/tool/other"
 	"errors"
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"strings"
 )
 
 type WebServer struct {
@@ -63,7 +66,11 @@ func (r *WebServer) Do(params map[string]any) (resp []byte) {
 	return []byte("服务启动失败:" + err.Error())
 }
 
-func handler1(req map[string]any) (resp []byte) {
+/*
+*
+web请求处理逻辑
+*/
+func handler1(req map[string]any) []byte {
 	param, ok := req["params"].(string)
 	if !ok {
 		return []byte("参数param不能为空")
@@ -72,6 +79,62 @@ func handler1(req map[string]any) (resp []byte) {
 	if len(commands) <= 0 {
 		return []byte("参数param不能为空")
 	}
+	resp, b := requestForward(commands)
+	if b {
+		return resp
+	}
 	commands = append([]string{"main.go"}, commands...)
 	return basic.Servlet(commands, false)
+}
+
+/*
+*
+web请求转发
+*/
+func requestForward(args []string) ([]byte, bool) {
+	var addr, params string
+	var flag bool
+	//判断是否需要转发，并拼接转发参数
+	for i := 0; i < len(args); i++ {
+		if args[i] == "--addr" {
+			flag = true
+			i++
+			if i < len(args) {
+				addr = args[i]
+			}
+			if addr == "" {
+				msg := fmt.Sprintf("请求转发的ip端口为空")
+				log.Error(msg)
+				return []byte(msg), true
+			}
+		} else {
+			params = args[i] + " "
+		}
+	}
+	if !flag {
+		return nil, flag
+	}
+	//校验转发地址是否合法
+	msg := fmt.Sprintf("地址不合法")
+	arr := strings.Split(addr, ":")
+	if len(arr) != 2 {
+		log.Error(msg)
+		return []byte(msg), flag
+	}
+	ipflag := othertool.CheckIp(arr[0])
+	portflag := othertool.CheckPortByString(arr[1])
+	if !ipflag || !portflag {
+		log.Error(msg)
+		return []byte(msg), flag
+	}
+	//转发请求并返回结果
+	uri := fmt.Sprintf("http://%s/do", addr)
+	resp, err := net.PostRespString(uri, map[string]string{
+		"params": params,
+	}, nil)
+	if err != nil {
+		return []byte(err.Error()), flag
+	} else {
+		return []byte(resp), flag
+	}
 }
