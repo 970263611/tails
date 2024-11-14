bed14329d3de12e99d7e2eff08ae0b2750b2f137
zhh2024 <15536508342@163.com>
2024-11-14 10:06:12 +0800

    sql新增查询导出到指定文件功能

--- extension/sql_server/sql_server_test.go
+++ extension/sql_server/sql_server_test.go
@@ -14,6 +14,7 @@ func TestDoHandler(t *testing.T) {
 		"password":   "postgre",
 		"dbname":     "postgres",
 		"searchPath": "public",
+		"outPutFile": "D:\\zhh.txt",
 		"sql":        "SELECT * FROM test",
 	}
 	instance := GetInstance()
