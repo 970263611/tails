54acd3be04b437b16ddc8fae481f21ff5b282b93
zhh2024 <15536508342@163.com>
2024-12-10 16:35:15 +0800

    优化 url_on_off 服务,添加查询功能和可选线程数功能

--- extension/url_on_off/urlConst.go
+++ extension/url_on_off/urlConst.go
@@ -26,6 +26,8 @@ type findResult struct {
 	token     string
 	urlPrefix string
 	uri       string
+	count     int
+	liuKong   LiuKongQueryRespEntry
 }
 
 func (f findResult) login() (string, error) {
@@ -137,6 +139,7 @@ func (f findResult) createApiGroup() (*RespEntry, error) {
 
 	header := http.Header{}
 	header.Set("Authorization", f.token)
+	header.Set("Content-Type", "application/json")
 	var apiCreateRespEntry RespEntry
 	// 发送 POST 请求
 	err = net.PostRespStruct(f.urlPrefix+"/api/gateway-apidefinitions", postData, header, &apiCreateRespEntry)
@@ -197,8 +200,8 @@ func (f findResult) updateApiGroup(apiQuery *ApiQueryRespEntry) (*RespEntry, err
 	}
 
 	header := http.Header{}
-	header.Set("Content-Type", "application/json")
 	header.Set("Authorization", f.token)
+	header.Set("Content-Type", "application/json")
 	var apiUpdateRespEntry RespEntry
 	err = net.PutRespStruct(f.urlPrefix+"/api/gateway-apidefinitions", nil, postData, header, &apiUpdateRespEntry)
 	if err != nil {
@@ -230,30 +233,46 @@ func (f findResult) queryLiuKong() (*LiuKongQueryRespEntry, error) {
 
 // 创建流控
 func (f findResult) createLiuKong() (*RespEntry, error) {
-	liuKong := map[string]any{
-		"resourceMode":         1,
-		"resource":             f.apiName,
-		"controlBehavior":      0,
-		"parseStrategy":        0,
-		"fieldName":            "",
-		"prop":                 false,
-		"grade":                0,
-		"count":                0.0,
-		"intervalSec":          1,
-		"region":               "秒",
-		"burst":                0,
-		"matchStrategy":        0,
-		"routeName":            f.apiName,
-		"matchType":            "normal",
-		"index":                0,
-		"maxQueueingTimeoutMs": 1,
-		"gatewayId":            f.gatewayId,
+	liuKong := LiuKongQueryRespEntry{
+		ResourceMode:         1,
+		Resource:             f.apiName,
+		ControlBehavior:      0,
+		ParseStrategy:        0,
+		FieldName:            "",
+		Prop:                 false,
+		Grade:                0,
+		Count:                float32(f.count),
+		IntervalSec:          1,
+		Region:               "秒",
+		Burst:                0,
+		MatchStrategy:        0,
+		RouteName:            f.apiName,
+		MatchType:            "normal",
+		Index:                0,
+		MaxQueueingTimeoutMs: 1,
+		GatewayId:            f.gatewayId,
 	}
 	header := http.Header{}
 	header.Set("Authorization", f.token)
+	header.Set("Content-Type", "application/json")
 	var liuKongCreateRespEntry RespEntry
 	// 发送 POST 请求
 	err := net.PostRespStruct(f.urlPrefix+"/api/gateway-flow-rules", liuKong, header, &liuKongCreateRespEntry)
+	if err != nil {
+		return nil, err
+	}
+	return &liuKongCreateRespEntry, nil
+}
+
+func (f findResult) updateLiuKong() (*RespEntry, error) {
+	liuKong := f.liuKong
+	liuKong.Count = float32(f.count)
+	header := http.Header{}
+	header.Set("Authorization", f.token)
+	header.Set("Content-Type", "application/json")
+	var liuKongCreateRespEntry RespEntry
+	// 发送 POST 请求
+	err := net.PutRespStruct(f.urlPrefix+"/api/gateway-flow-rules", nil, liuKong, header, &liuKongCreateRespEntry)
 	if err != nil {
 		return nil, err
 	}
